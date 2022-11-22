22a0f0abd16bfc7fdf36695cfb9dd14a76977b66
Pranav-Bobde <bobdep31@gmail.com>
2022-11-22 20:56:12 +0530

    base client app

---
+++ client/src/App.tsx
@@ -0,0 +1,15 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import "./App.css";
+
+function App(): JSX.Element {
+  return (
+    <div className="App">
+      <h1 className="text-xl h-screen flex flex-col justify-center">
+        React Todo
+      </h1>
+    </div>
+  );
+}
+
+export default App;
